209d0fbc4b9ba51ce0c6c1a4c7b9b70d741a180d
Galkina-Darya <99248872+Galkina-Darya@users.noreply.github.com>
2022-06-09 19:26:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,24 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> Tree;
+  std::ifstream file (filename);
+  if (!file) {
+    throw std::string ("File is not found!");
+  }
+  std::string word = "";
+  while (!file.eof()) {
+    char chr = file.get();
+    int ascii = chr;
+    if ((ascci >= 65 && ascii <= 90) || (ascci >= 97 && ascii <= 122)) {
+      word += tolower(ascii);
+    } else {
+      if (word != "") {
+        tree.add(word);
+        word = "";
+      }
+    }
+  }
+  file.close();
+  return tree;
 }
